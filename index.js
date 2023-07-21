6ac3a47d142cb93ce304e53dbf4f568b7ac18c90
Misael Sena <misaellsena@gmail.com>
2023-07-20 23:48:34 -0300

    Resposta ao Usuário Implementada

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
-var resposta = prompt("Você está com dificuldades na utilização do Git?: ");
+/* Indizindo o aluno com dificulade a assitir a 
+ * aula do Youtube indicada pelo professor.
+ */
+
+    function analiseResposta(){
+        
+        var resposta = prompt("Você está com dificuldades na utilização do Git?(Sim/Não): ");
+    
+    
+        if(resposta.toLowerCase() === "sim"){
+            console.log("Assista e pratique a aula do Youtube indicada pelo professor! ");        
+        }else if(resposta.toLowerCase() === "não"){
+            console.log("Parabéns! Continue praticando pois Git é essencial na vida de um programador! Bons Estudos!");
+        }else{
+            console.log("Responda Apenas sim ou não!");
+            return analiseResposta();       
+        }
+    }
+    analiseResposta();
